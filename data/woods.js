898987323152bbd4a278729ef74522fca4c2b378
Raquel Bujans <raquel.bujans@gmail.com>
2017-01-22 20:35:10 -0500

    adding first pass at app

---
+++ data/woods.js
@@ -0,0 +1,30 @@
+[
+  {
+    name: 'Maple',
+    color: '#',
+  },
+  {
+    name: 'Cherry',
+    color: '#',
+  },
+  {
+    name: 'Maple',
+    color: '#',
+  },
+  {
+    name: 'Poplar',
+    color: '#',
+  },
+  {
+    name: 'Bamboo',
+    color: '#',
+  },
+  {
+    name: 'Oak',
+    color: '#',
+  },
+  {
+    name: 'Walnut',
+    color: '#',
+  },
+]
